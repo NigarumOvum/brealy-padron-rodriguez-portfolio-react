af950668b027ae7017dcbc5ff4745e9a95a90b46
nigarumovum <bfpr131095@gmail.com>
2021-11-08 08:36:28 -0600

    123123123123

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -14,7 +14,7 @@ function ProjectCard() {
     showStatus={false}
     useKeyboardArrows={true}
     centerMode={false}
-    centerSlidePercentage={110}
+    centerSlidePercentage={50}
     showArrows={true}
     showThumbs={false}
   >
