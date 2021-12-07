23c41f22aad82ce23eedd6c47497baa3a35810b1
nigarumovum <bfpr131095@gmail.com>
2021-12-07 08:18:23 -0600

    updateinfo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,52 +10,48 @@ import Gradient from 'rgt';
 export function Header() {
   return (
     <>
-     <section id='header-mainpage'>
-      <div className='header-mainpage-container'>
-        <div className='header-mainpage-title'>
-        <h1>My name is </h1>
-          <img src={ProfileImg} alt=""/>
-          <h1> <strong>Brealy Padrón Rodríguez</strong></h1>
-          <h1>...and <strong>
-          <Gradient dir="left-to-right" from="#00DFD8" to="#007CF0">
-            I'm
-            </Gradient>
+      <section id='header-mainpage'>
+        <div className='header-mainpage-container'>
+          <div className='header-mainpage-title'>
+            <h1>My name is </h1>
+            <img src={ProfileImg} alt="" />
+            <h1> <strong>Brealy Padrón Rodríguez</strong></h1>
+            <h1>...and <strong>
+              <Gradient dir="left-to-right" from="#00DFD8" to="#007CF0">
+                I'm
+              </Gradient>
             </strong></h1>
-             <h2>
-              <Typewriter 
-              onInit={(typewriter) => {
-                typewriter.typeString(' a Full-Stack <strong>Developer</strong>...')
-                .pauseFor(1500)
-                .deleteAll()
-                typewriter.typeString(' and a Site Reliability<strong> Engineer (DevOps)</strong>...')
-                .pauseFor(3500)
-                .start()
-              }}
-              options={{
-                delay: 60,
-                autoStart: true,
-                loop: true,
-                cursor: '|',
-                wrapperClassName: 'header-mainpage-title-type-wraper',
-              }}
-            />
+            <h2>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString(' a Full-Stack <strong>Developer</strong>...')
+                    .pauseFor(1500)
+                    .deleteAll()
+                  typewriter.typeString(' and a Site Reliability<strong> Engineer (DevOps)</strong>...')
+                    .pauseFor(3500)
+                    .start()
+                }}
+                options={{
+                  delay: 60,
+                  autoStart: true,
+                  loop: true,
+                  cursor: '|',
+                  wrapperClassName: 'header-mainpage-title-type-wraper',
+                }}
+              />
             </h2>
             <p>
-              I consider myself as <strong> a passionate computer science student</strong>, always excited about the new technology trends.  
-            <strong> I have experience building Web Apps</strong> with <strong>OOP</strong> (Object Oriented Programming) languages like <strong>Python, </strong> 
-            & <strong>JavaScript </strong> using  <strong> ORM </strong>(Object-Relational Mapper), Class-Based and Functional Programming, 
-             following  <strong> DDD</strong> (domain-driven design),<strong> MVT</strong> (Model View Template), <strong>MVC </strong>(Model Views Controller) &
-            <strong> Hexagonal/Clean Architectures</strong>
-             </p>
-        </div>
-    
+              I consider myself as a passionate computer science student, always curious and excited about the new technology trends with a relentless attention to details. With strong bias for action and ability to operate independently, communication and organization skills with an ability to thrive in a fast-paced production deadline-driven environment.
+            </p>
+          </div>
+
 
           <div className='header-buttons'>
             <a className='continuesee-button' href="/#projects">Projects</a>
             <Link to="/about" className='seemore-button'>More about my skills</Link>
           </div>
 
-      </div>      
+        </div>
       </section>
 
     </>
--- src/pages/about/Sobre.tsx
+++ src/pages/about/Sobre.tsx
@@ -33,39 +33,71 @@ function Sobre() {
           <div className='sobre-card-information'>
             
             <TiUserOutline className='sobre-card-personicon' size={60} />
-            <SobreInfo text='I consider myself an enthusiastic learner, and active student with a keen interest in new technological trends, in helping others find the most effective solution to their technical problems. In addition, I always seek progress through the acquisition of new skills and knowledge' />
+            <SobreInfo 
+            text=
+    'I have experience building Web Applications based in OOP (Object Oriented Programming), following MVT (Model View Template), MVC (Model Views Controller) using ORM (Object-Relational Mapper) to manage & model DBs, also DDD (domain-driven design), Hexagonal & Clean Architectures. Using Class-Based and Functional Programming.' 
+          />
+            <hr />
+            <SobreInfo
+            text= 'For the front-end I usually work with popular frameworks like jQuery/React/Vue.'
+            />
             
+            <p> Great Ability to take visual UX designs (PSD files, AdobeXD, Anima, Figma, Sketch designs) and convert them into semantic and standard compliant web properties.  </p>
+            <p>  Build cross-browser and cross-device compatible pages according to W3C standards. </p>
+            <p>   I also make the web pretty by using CSS (FlexBox/Grid) and SASS/LESS prepocessors. Do responsiveness across all cross-platform devices UI layouts. </p>
+            <p>  I have good understanding of On-Page SEO principles. </p>
+            <p>   Solid experience in using a complex REST API from the client-side </p>
+            <p>   Significant experience of troubleshooting using JavaScript-based tools like Chrome Developer Console. </p>
+            <hr />
+
+             <SobreInfo
+            text=
+            'For the back-end I have experience in Django/Python, Node/Express.'
+            />
+          
+            <SobreInfo
+            text=
+            'For the DBs I know how to work with MySQL, MongoDB, PostgreSQL, MariaDB, Redis using Python/JavaScript'
+/>
+<SobreInfo
+            text=
+            ' Exposure to DevOps and Continuous Integration/Delivery pipeline, including Git, Jenkins, etc.'
+            />
             <BiCodeAlt className='sobre-card-personicon' size={60} />
 
             <SobreInfo text={`I have projects and experience in the most popular stacks: `} />   
-
-            <SobreInfo text={`LAMP Stack: Linux | Apache | MySQL | PHP`} />    
+<h1> LAMP Stack: </h1>
+            <SobreInfo text={`Linux | Apache | MySQL | PHP`} />    
             { <img src="https://img.icons8.com/color/96/000000/linux--v2.png" alt="12"/>}
             <SiApache className='apa-icon' size={96} />
             { <img src="https://img.icons8.com/color/96/000000/mysql-logo.png" alt="12"/> }
             { <img src="https://img.icons8.com/dusk/96/000000/php-logo.png" alt="12"/> }
-            <SobreInfo text={`LEMP Stack: Linux | Nginx | MySQL | PHP`} />          
+            <h1> LEMP Stack: </h1>
+            <SobreInfo text={`Linux | Nginx | MySQL | PHP`} />          
             { <img src="https://img.icons8.com/color/96/000000/linux--v2.png" alt="12"/>}
             { <img src="https://img.icons8.com/color/96/000000/nginx.png" alt="12"/> } 
             { <img src="https://img.icons8.com/color/96/000000/mysql-logo.png" alt="12"/> }
             { <img src="https://img.icons8.com/dusk/96/000000/php-logo.png" alt="12"/> }
-            <SobreInfo text={`Python stack: Django | HTML5 | CSS3 | JavaScript | MySQL`} /> 
+            <h1> Python Stack: </h1>
+            <SobreInfo text={`Django | HTML5 | CSS3 | JavaScript | MySQL`} /> 
             {<img src="https://img.icons8.com/fluency/65/000000/python.png" alt="12"/> }
             { <img src="https://img.icons8.com/color/65/000000/django.png" alt="12" /> }
             { <img src="https://img.icons8.com/color/65/000000/html-5--v1.png" alt="12" /> }
             { <img src="https://img.icons8.com/color/65/000000/css3.png" alt="12"/> }
             { <img src="https://img.icons8.com/color/65/000000/javascript--v1.png"alt="12" /> }
             { <img src="https://img.icons8.com/color/65/000000/mysql-logo.png" alt="12"/> }
+            <h1> MERN JavaScript Stack: </h1>
 
-            <SobreInfo text={`MERN JavaScript stack- MongoDB/MySQL - Express -  React - Node.js`} /> 
-            { <img src="https://img.icons8.com/color/96/000000/javascript--v1.png" alt="12"/> }
+            <SobreInfo text={`MongoDB/MySQL - Express -  React - Node.js`} /> 
             { <img src="https://img.icons8.com/color/96/000000/mongodb.png" alt="12"/> }
+            { <img src="https://img.icons8.com/color/96/000000/javascript--v1.png" alt="12"/> }
             { <img src="https://img.icons8.com/plasticine/100/000000/react.png" alt="12"/> }
             { <img src="https://img.icons8.com/fluency/96/000000/node-js.png" alt="12"/>}
+            <h1> MEVN JavaScript Stack: </h1>
 
-            <SobreInfo text={`MEVN JavaScript stack: - MongoDB/MySQL - Express -  Vue - Node.js`} />    
-            { <img src="https://img.icons8.com/color/96/000000/javascript--v1.png" alt="12"/> }
+            <SobreInfo text={`MongoDB/MySQL - Express -  Vue - Node.js`} />    
             { <img src="https://img.icons8.com/color/96/000000/mongodb.png" alt="12"/> }
+            { <img src="https://img.icons8.com/color/96/000000/javascript--v1.png" alt="12"/> }
             { <img src="https://img.icons8.com/color/96/000000/vue-js.png" alt="12"/> }
             { <img src="https://img.icons8.com/fluency/96/000000/node-js.png" alt="12"/>}  
 
