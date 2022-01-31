462e4ba089c5f721db614d9d32bc85bafb411ef1
Brealy Fabian Padron Rodriguez <48064359+NigarumOvum@users.noreply.github.com>
2022-01-30 20:45:18 -0600

    Update Sobre.tsx

--- src/pages/about/Sobre.tsx
+++ src/pages/about/Sobre.tsx
@@ -114,7 +114,8 @@ function Sobre() {
               
 <SobreInfo text={`JavaScript:`} /> <p> ES5/ES6 Syntax, scope, IIFE, hoisting, closures, callbacks(ES5), promises(ES6) , async/wait(ES7), DOM manipulation, AJAX, jQuery, Fetch, Developer Console Debugging, complex RPC/REST APIs, Write unit, integration, and functional test.  </p>
 
-<SobreInfo text=
+<SobreInfo text={`Package Managers:`} /> <p> npm/yarn/bower  </p>
+              
               
 
 
